3a1eb40310b99989b4e0121e5f31ee75c3960c55
Pepe <you@example.com>
2025-05-24 12:27:26 -0600

    Changed session from path to session name

--- src/Session.php
+++ src/Session.php
@@ -15,37 +15,39 @@ namespace ocallit\Util;
 
 class Session {
 
-    public function __construct(string $cookiePath = '/', int $lifetimeSeconds = 30* 60 * 60) {
+    public function __construct(string $sessionName = '/', int $lifetimeSeconds = 30* 60 * 60) {
         if(session_status() == PHP_SESSION_NONE) {
-            $this->startSecureSession($cookiePath,$lifetimeSeconds);
+            $this->start($sessionName,$lifetimeSeconds);
         }
     }
 
-    protected function startSecureSession(string $cookiePath, int $lifetimeSeconds): void {
+    protected function start(string $sessionName, int $lifetimeSeconds): void {
         ini_set('session.gc_maxlifetime', $lifetimeSeconds);
         ini_set('session.cookie_lifetime', $lifetimeSeconds);
         ini_set('session.gc_probability', 1);
         ini_set('session.gc_divisor', 1000);
         $session_options = [
-          'cookie_secure' => TRUE, // Send cookie only over HTTPS
+          'cookie_secure' => $this->isHTTPS(), // Send cookie only over HTTPS
           'cookie_httponly' => TRUE, // Prevent JavaScript access to cookie
           'use_strict_mode' => TRUE, // Prevent session fixation
           'cookie_samesite' => 'Strict', // Prevent CSRF attacks
           'cookie_lifetime' => $lifetimeSeconds,
         ];
-
-        $session_options['cookie_path'] = $cookiePath; // Explicitly set path to root
-        $session_options['sid_length'] = 128; // Increase session ID length
-        $session_options['sid_bits_per_character'] = 6; // Use more bits per character in session ID
-
+        session_name($sessionName);
         session_start($session_options);
-        $_SESSION['_path'] = $cookiePath; // Store the path in session
+        $_SESSION['_session_name'] = $sessionName; // Store the path in session
     }
 
-    public function isLoggedIn(): bool {return !empty( $_SESSION[($_SESSION['_path'] ?? "") . "\t" . 'loggedin']); }
+    protected function isHTTPS():bool {
+        return
+          ($_SERVER['HTTPS'] ?? "") === "on" ||
+          ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? "") === 'https' ||
+          ($_SERVER['HTTP_X_FORWARDED_SSL'] ?? "") === 'on' || ($_SERVER['SERVER_PORT'] ?? "") === '443';
+    }
+    public function isLoggedIn(): bool {return !empty( $_SESSION[($_SESSION['_session_name'] ?? "") . "\t" . 'loggedin']); }
 
     public function login(array $sessionVars = []): void {
-        $_SESSION[$_SESSION['_path'] . "\t" . 'loggedin'] = TRUE;
+        $_SESSION[$_SESSION['_session_name'] . "\t" . 'loggedin'] = TRUE;
         foreach($sessionVars as $key => $value)
             $_SESSION[$key] = $value;
         session_regenerate_id(TRUE); // Regenerate session ID after login
